de838e70d49cc21d1415da84b5fc5f5d01087d4f
NastyaZhalova <nast.zhal@gmail.com>
2025-09-08 13:11:26 +0300

    Итоговая

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,4 +1,5 @@
 import org.example.LogCalculator;
+import org.example.Main;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import java.math.BigDecimal;
@@ -7,18 +8,18 @@ import java.math.RoundingMode;
 
 public class MainTest {
 
-    private double roundToThree(double value) {
-        return new BigDecimal(value)
-                .setScale(3, RoundingMode.HALF_UP)
-                .doubleValue();
-    }
+//    private double roundToThree(double value) {
+//        return new BigDecimal(value)
+//                .setScale(3, RoundingMode.HALF_UP)
+//                .doubleValue();
+//    }
 
     @Test
     public void testLogAccuracy() {
         int k = 4;
         double x = 0.5;
-        double expected = roundToThree(Math.log(1 - x));
-        double actual = roundToThree(LogCalculator.calculate(k, x));
+        double expected = Main.roundToThree(Math.log(1 - x));
+        double actual = Main.roundToThree(LogCalculator.calculate(k, x));
 
         assertEquals(expected, actual,
                 "Ожидаемое значение должно совпадать с округлённым результатом Log()");
@@ -28,8 +29,8 @@ public class MainTest {
     public void testLogNearZero() {
         int k = 6;
         double x = 0.0001;
-        double expected = roundToThree(Math.log(1 - x));
-        double actual = roundToThree(LogCalculator.calculate(k, x));
+        double expected = Main.roundToThree(Math.log(1 - x));
+        double actual = Main.roundToThree(LogCalculator.calculate(k, x));
 
         assertEquals(expected, actual,
                 "Ожидаемое значение должно совпадать с округлённым результатом Log()");
@@ -39,8 +40,8 @@ public class MainTest {
     public void testLogAtEdge() {
         int k = 12;
         double x = 0.999;
-        double expected = roundToThree(Math.log(1 - x));
-        double actual = roundToThree(LogCalculator.calculate(k, x));
+        double expected = Main.roundToThree(Math.log(1 - x));
+        double actual = Main.roundToThree(LogCalculator.calculate(k, x));
 
         assertEquals(expected, actual,
                 "Ожидаемое значение должно совпадать с округлённым результатом Log()");
@@ -50,8 +51,8 @@ public class MainTest {
     public void testZeroInput() {
         int k = 3;
         double x = 0.0;
-        double expected = roundToThree(Math.log(1 - x)); // log(1) = 0
-        double actual = roundToThree(LogCalculator.calculate(k, x));
+        double expected = Main.roundToThree(Math.log(1 - x));
+        double actual = Main.roundToThree(LogCalculator.calculate(k, x));
 
         assertEquals(expected, actual,
                 "Ожидаемое значение должно совпадать с округлённым результатом Log()");
